8a4102f6b8e1e73fa77ea17c39acf054d883236b
Aninoss <leonblech@tutanota.com>
2023-01-05 16:41:24 +0100

    Proxy timeout improved

--- src/main/java/core/HttpClient.java
+++ src/main/java/core/HttpClient.java
@@ -1,7 +1,5 @@
 package core;
 
-import java.io.InterruptedIOException;
-import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -15,28 +13,20 @@ public class HttpClient {
     public static final String USER_AGENT = "Lawliet Discord Bot by Aninoss#7220";
 
     private final OkHttpClient client = new OkHttpClient.Builder()
-            .connectTimeout(10, TimeUnit.SECONDS)
-            .writeTimeout(10, TimeUnit.SECONDS)
-            .readTimeout(10, TimeUnit.SECONDS)
+            .callTimeout(10, TimeUnit.SECONDS)
             .build();
 
     public HttpResponse request(String url) {
         String domain = url.split("/")[2];
-        try (AsyncTimer timer = new AsyncTimer(Duration.ofSeconds(10))) {
-            Request request = new Request.Builder()
-                    .url(url)
-                    .addHeader("User-Agent", USER_AGENT)
-                    .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("User-Agent", USER_AGENT)
+                .build();
 
-            try (Response response = client.newCall(request).execute()) {
-                return new HttpResponse()
-                        .setCode(response.code())
-                        .setBody(response.body().string());
-            }
-        } catch (InterruptedIOException e) {
-            LOGGER.error("Web time out ({})", domain);
+        try (Response response = client.newCall(request).execute()) {
             return new HttpResponse()
-                    .setCode(500);
+                    .setCode(response.code())
+                    .setBody(response.body().string());
         } catch (Throwable e) {
             LOGGER.error("Web error ({})", domain, e);
             return new HttpResponse()
